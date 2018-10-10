2f21d322737bc121e512c0cddd827d96b7d83767
MiroslavaTatsenko <tmira@ukr.net>
2018-10-10 16:30:12 +0300

    create models

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               blank=True, 
+                               null=True)
+    image = models.ImageField(upload_to='images', blank=False)
+    name = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=512, blank=False)
+
+    def _get_count_children_group(self):
+        return Group.objects.filter(parent=self.id).count()
+
+    def _get_count_children_item(self):
+        return Item.objects.filter(parent=self.id).count()
+    
+    count_children_group = property(_get_count_children_group)
+    count_children_item = property(_get_count_children_item)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Item(models.Model):
+    parent = models.ForeignKey(Group,
+                               on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', blank=False)
+    name = models.CharField(max_length=64, blank=False)
+    description = models.CharField(max_length=512, blank=False)
+    date = models.DateField(blank=False)
+    check = models.BooleanField(blank=False, 
+                                null=True)
+    
+    def __str__(self):
+        return f'{self.name}'
