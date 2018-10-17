002ee7667036fb3e2ed25829d918e96cc3594a56
MiroslavaTatsenko <tmira@ukr.net>
2018-10-17 23:38:05 +0300

    del views, add apiviews

--- my_app/views.py
+++ my_app/views.py
@@ -1,75 +1,40 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
-from django.views.generic import CreateView
-from datetime import datetime
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-from .models import Item, Group
-from .forms import ItemForm
+from .models import Group, Item
+from .serializers import GroupSerializer, ItemSerializer, GroupDetailSerializer
 
-def index(request):
-    # found list parent groups
-    list_parent_group = Group.objects.filter(parent=None).order_by('id')
-    current_page = Paginator(list_parent_group, 10)
-    page = request.GET.get('page', 1)
-    try:
-        list_group = current_page.page(page)
-    except PageNotAnInteger:
-        list_group = current_page.page(1)
-    except EmptyPage:
-        list_group = current_page.page(current_page.num_pages)
-    return render(request, 
-                  'my_app/index.html',
-                  {'list_group': list_group})
 
-def show_group(request, group_id):
-    current_group = Group.objects.get(id=group_id)
+class GroupList(generics.ListAPIView):
+    queryset = Group.objects.filter(parent=None)
+    serializer_class = GroupSerializer
 
-    # found list children groups current group 
-    list_parent_group = Group.objects.filter(parent=group_id).order_by('id')
-    current_page_group = Paginator(list_parent_group, 10)
-    page_group = request.GET.get('page_group', 1)
-    try:
-        list_group = current_page_group.page(page_group)
-    except PageNotAnInteger:
-        list_group = current_page_group.page(1)
-    except EmptyPage:
-        list_group = current_page_group.page(current_page_group.num_pages)
 
-    # found list children inem current group 
-    list_parent_item = Item.objects.filter(parent=group_id).filter(check=True).order_by('id')
-    current_page_item = Paginator(list_parent_item, 10)
-    page_item = request.GET.get('page_item', 1)
-    try:
-        list_item = current_page_item.page(page_item)
-    except PageNotAnInteger:
-        list_item = current_page_item.page(1)
-    except EmptyPage:
-        list_item = current_page_item.page(current_page_item.num_pages)
+class GroupDetail(generics.RetrieveAPIView):
+    queryset = Group.objects.all()
+    serializer_class =  GroupDetailSerializer
 
-    return render(request,
-                  'my_app/show_group.html',
-                  {'current_group': current_group,
-                   'list_group': list_group,
-                   'list_item': list_item})
 
-def show_item(request, item_id):
-    current_item = Item.objects.get(id=item_id)
-    return render(request,
-                  'my_app/show_item.html',
-                  {'current_item': current_item})
+class ItemList(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
 
-def add_new_item(request, parent_id):
-    if request.method == "POST":
-        form = ItemForm(request.POST, request.FILES)
-        print(request.FILES)
-        if form.is_valid():
-            item = form.save(commit=False)
-            item.parent = Group.objects.get(id=parent_id)
-            item.date = datetime.now()
-            item.check = None
-            item.save()
-            return redirect('show_group', group_id=parent_id)
-    form = ItemForm()
-    return render(request,
-                  'my_app/add_new_item.html',
-                  {'form': form})
+    def get_queryset(self):
+        print(self.kwargs)
+        pk = self.kwargs['pk']
+        return Item.objects.filter(check=True).filter(parent=pk)
+
+    def create(self, request, *args, **kwargs):
+        # Create valide data
+        data = dict(request.data)
+        for x in data:
+            data[x] = data[x][0]
+        del data['csrfmiddlewaretoken']
+        data['parent'] = str(kwargs['pk'])
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid()
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        print(headers)
+        return Response(serializer.data, headers=headers)
