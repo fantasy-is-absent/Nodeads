d8b09dcc2058bafb7d6cca39421f7673008aa841
MiroslavaTatsenko <tmira@ukr.net>
2018-10-17 23:37:10 +0300

    add serializers

---
+++ my_app/serializers.py
@@ -0,0 +1,66 @@
+import json
+from rest_framework import serializers
+
+from .models import Group, Item
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    count_children_group = serializers.IntegerField()
+    count_children_item = serializers.IntegerField()
+    detail_url = serializers.HyperlinkedIdentityField(view_name='group_detail')
+
+    class Meta:
+        model = Group
+        fields = ('id', 
+                  'parent', 
+                  'image', 
+                  'name',
+                  'description',
+                  'count_children_group', 
+                  'count_children_item',
+                  'detail_url', 
+                  )
+        read_only_fields = ('count_children_group', 
+                            'count_children_item',
+                            'detail_url',
+                           )
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Item
+        fields = ('parent',
+                  'image',
+                  'name', 
+                  'description',
+                  'date',
+                 )
+        read_only_fields = ( 'date',)
+
+
+class GroupDetailSerializer(GroupSerializer):
+    get_children_group_list = serializers.ListField(
+        child = GroupSerializer()
+        )
+    get_children_item_list = serializers.ListField(
+        child = ItemSerializer()
+        )
+    
+
+    class Meta:
+        model = Group
+        fields = ('id', 
+                  'parent', 
+                  'image',
+                  'name', 
+                  'description',
+                  'count_children_group', 
+                  'count_children_item', 
+                  'get_children_group_list', 
+                  'get_children_item_list',
+                  )
+        read_only_fields = ('count_children_group', 
+                            'count_children_item', 
+                            'get_children_group_list', 
+                            'get_children_item_list',)
